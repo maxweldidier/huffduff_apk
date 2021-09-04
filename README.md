232ca5630ba408ced1a39a67f0e0595854fbd01b
maxwel didier <maxweldidier1@gmail.com>
2021-09-04 13:39:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# project huffduff
+ HuffDuff 
---
+++ lib/splashscreen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InitState();
+}
+
+class InitState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = Duration(seconds: 3);
+    return new Timer(duration, loginRoute);
+  }
+
+  loginRoute() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => LoginScreen()
+    ));
+  }
+  @override
+  Widget build(BuildContext context){
+    return initWidget(context);
+  }
+
+  Widget initWidget(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: new Color(0xFF1FD5F5),
+                gradient: LinearGradient(
+                    colors: [(new Color(0xFF1FF5F5)), new Color(0xFF1EF2F2)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter)),
+          ),
+          Center(
+            child: Container(
+              child: Image.asset("images/launch.png"),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
